71bab61b2fdab5fdea9f2b6491d77f95fa6f7802
Joe Durand <joe.durand3@gmail.com>
2018-03-08 08:13:21 -0800

    init commit - VS solution and keyboard driver

---
+++ ASLKeyboard/ASLKeyboard/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ASLKeyboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("typing...");
+                var test = new Keyboard();
+                test.SendInput(Keyboard.ScanCodeShort.KEY_0);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
